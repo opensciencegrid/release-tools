e3f56b05f7b6ceaf56313be29ab0525c78d6e9a0
Brian Lin <blin@cs.wisc.edu>
2013-11-12 21:15:11 +0000

    Start tracking release scripts
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/release-tools@18181 4e558342-562e-0410-864c-e07659590f8c

---
+++ release-common.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+die () {
+    echo "$@" 1>&2
+    exit 1
+}
+
+usage () {
+    echo "usage: `basename $0` [options] VERSION"
+    echo "Options:"
+    echo -e "\t-h, --help\tPrint this message"
+    echo -e "\t-u, --upcoming\tAlso populate upcoming pre-release"
+}
+
+if [ $# -lt 1 ]; then
+    usage
+    die
+fi
+
+upcoming=0
+
+while [ $# -ne 0 ];
+do
+    case $1 in
+        -h|--help)
+            usage
+            die
+            ;;
+        -u|--upcoming)
+            upcoming=1
+            shift
+            ;;
+        -*)
+            usage
+            die "unknown option: $1"
+            ;;
+        *)
+            if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+                version=$1
+                shift
+            else
+                usage
+                die "unkown parameter: $1"
+            fi
+    esac
+done
+
+majorver=`sed -r 's/\.[0-9]+$//' <<< $version`
+osgver="osg`sed 's/\.//' <<< $majorver`"
+
+branches=($majorver)
+if [ $upcoming -eq 1 ]; then
+    branches+=('upcoming')
+fi
